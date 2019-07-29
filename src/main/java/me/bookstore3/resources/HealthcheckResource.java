83c649d6d0a7811d86b7043bf4ed34d67241d2a5
salamtamp <salamtam.p@gmail.com>
2019-07-29 21:25:32 +0700

    Add postman test

--- src/main/java/me/bookstore3/resources/HealthcheckResource.java
+++ src/main/java/me/bookstore3/resources/HealthcheckResource.java
@@ -1,18 +1,23 @@
 package me.bookstore3.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import me.bookstore3.services.HealthcheckService;
+
 @RestController
 public class HealthcheckResource {
+	@Autowired
+	private HealthcheckService healthcheckService;
 	
 	@GetMapping(path="/")
 	public String status() {
-		return "ok";
+		return healthcheckService.getMessage();
 	}
 	
 	@GetMapping(path="/healthcheck")
 	public String healthcheck() {
-		return "ok";
+		return healthcheckService.getMessage();
 	}
 }
